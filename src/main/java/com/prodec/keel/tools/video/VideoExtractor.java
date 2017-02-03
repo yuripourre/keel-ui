76831913a7d24295022ab48580cacb9396498226
Yuri Pourre <yuripourre@gmail.com>
2017-02-03 15:53:29 -0200

    Aplica filtro de máscara no extrator de video

--- src/main/java/com/prodec/keel/tools/video/VideoExtractor.java
+++ src/main/java/com/prodec/keel/tools/video/VideoExtractor.java
@@ -4,6 +4,7 @@ import java.awt.Color;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.imageio.ImageIO;
@@ -17,16 +18,28 @@ import br.com.etyllica.core.event.KeyEvent;
 import br.com.etyllica.core.event.MouseEvent;
 import br.com.etyllica.core.event.PointerEvent;
 import br.com.etyllica.core.graphics.Graphics;
+import br.com.etyllica.core.linear.Point2D;
 import br.com.etyllica.layer.Layer;
+import br.com.etyllica.loader.image.ImageLoader;
+import br.com.etyllica.motion.core.source.BufferedImageSource;
+import br.com.etyllica.motion.feature.Component;
+import br.com.etyllica.motion.filter.MaskFilter;
+import br.com.etyllica.motion.filter.validation.MinDensityValidation;
+import br.com.etyllica.motion.filter.validation.MinDimensionValidation;
 import br.com.etyllica.util.PathHelper;
+import br.com.etyllica.util.io.IOHelper;
 
 public class VideoExtractor extends Application {
 
+	private boolean imageLoaded = false;
 	private int speed = 200;
 	
 	private int totalFrames = 0;
-	private int frameNumber = 0;
+	private int frameNumber = 10000;
 	private BufferedImage frame;
+	private BufferedImage mask;
+	private BufferedImageSource source;
+	private Component screen;
 
 	private static final int NONE = -1;
 
@@ -42,6 +55,8 @@ public class VideoExtractor extends Application {
 	private final DashedStroke dash = new DashedStroke();
 
 	private boolean shouldExport = false;
+	MaskFilter filter;
+	List<Component> results = new ArrayList<Component>();
 
 	public VideoExtractor(int w, int h) {
 		super(w, h);
@@ -49,12 +64,14 @@ public class VideoExtractor extends Application {
 
 	@Override
 	public void load() {
-
+		mask = ImageLoader.getInstance().getImage("mask.jpg");
+		source = new BufferedImageSource();
 	}
 
 	@Override
 	public void draw(Graphics g) {
 		g.drawImage(frame, 0, 0);
+				
 		if (shouldExport) {
 			exportToFile(g.getBimg());
 			return;
@@ -64,6 +81,16 @@ public class VideoExtractor extends Application {
 		g.drawStringShadow("Frame: "+frameNumber+" / "+totalFrames, 10, 90);
 
 		drawSelection(g);
+		
+		g.setAlpha(60);
+		for (Component result : results) {
+			g.setColor(Color.BLUE);
+			g.drawRect(result.getRectangle());
+			for (Point2D point: result.getPoints()) {
+				g.fillRect((int)point.getX(), (int)point.getY(), 1, 1);
+			}
+		}
+		g.resetAlpha();
 	}
 
 	private void drawSelection(Graphics g) {
@@ -132,11 +159,14 @@ public class VideoExtractor extends Application {
 		}
 		if (event.isKeyDown(KeyEvent.VK_ENTER)) {
 			shouldExport = true;
+			dx = 0;
+			dy = 0;
+			dw = frame.getWidth();
+			dh = frame.getHeight();
 		}
 	}
 
 	private void exportToFile(BufferedImage image) {
-
 		Layer rect = getRectangle();
 		
 		BufferedImage img = new BufferedImage( 
@@ -152,7 +182,9 @@ public class VideoExtractor extends Application {
 			String path = PathHelper.currentDirectory()+"export"+File.separator;
 			String id = Long.toString(System.currentTimeMillis());
 			String directory = path + id;
+			
 			System.out.println(directory);
+			IOHelper.createDirectory(path);
 			
 			File file = new File(directory + ".jpg");
 			ImageIO.write(img, "jpg", file);
@@ -168,6 +200,17 @@ public class VideoExtractor extends Application {
 	public void dropFiles(int x, int y, List<File> files) {
 		file = files.get(0);
 		updateFrame();
+		
+		source.setImage(frame);
+		filter = new MaskFilter(source);
+		filter.setTolerance(27);
+		filter.addValidation(new MinDimensionValidation(10));
+		filter.addValidation(new MinDensityValidation(10));
+		filter.setMask(new BufferedImageSource(mask));
+		
+		screen = new Component(0, 0, source.getWidth(), source.getHeight());
+		
+		imageLoaded = true;
 	}
 
 	private void updateFrame() {
@@ -185,6 +228,12 @@ public class VideoExtractor extends Application {
 		} catch (JCodecException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+		}
+		
+		if (imageLoaded) {
+			source.setImage(frame);
+			results.clear();
+			results.addAll(filter.filter(source, screen));
 		}
 	}
 
